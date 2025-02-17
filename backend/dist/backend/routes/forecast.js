a4ca35edb05d6ea65fb24f293ae404986c6b81b2
Oxana Theis <otheis@protonmail.com>
2025-02-17 17:05:09 +0000

    Remove dist folder and update .gitignore

--- backend/dist/backend/routes/forecast.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { getForecast } from "../controllers/forecastController";
-const router = express.Router();
-router.get("/", getForecast);
-export default router;
--- backend/dist/backend/server.js
+++
@@ -1,8 +0,0 @@
-import http from "http";
-import app from "./app";
-const PORT = process.env.PORT || 13306;
-const server = http.createServer(app);
-server.keepAliveTimeout = 60000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
